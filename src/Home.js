bd9f8f14b09321e19f1d1e8145582800d57af1fe
Ankita <ankitapatil24042001@gmail.com>
2024-06-19 11:42:23 +0530

    push

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./styles.css";
-import card from "./card.jpg";
+import card from "./logo.jpg";
 
 export default function Home() {
   const navigate = useNavigate();
--- src/ScratchCardView.js
+++ src/ScratchCardView.js
@@ -1,23 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ScratchCard from "react-scratchcard-v2";
-import couponCover from "./card.jpg";
+import couponCover from "./logo.jpg";
 import "./styles.css";
-import bouquetImage from "./flowerbouquet.jpeg";
+import bouquetImage from "./FLowers.gif";
+// import balloonImage from "./flower.png"; // Import the balloon image
 
 export default function ScratchCardView() {
   const [scratchedText, setScratchedText] = useState("");
   const [discountPercentage, setDiscountPercentage] = useState("");
   const [couponNumber, setCouponNumber] = useState("");
   const [isScratched, setIsScratched] = useState(false);
+  const [showBalloons, setShowBalloons] = useState(false);
 
   const handleScratchComplete = () => {
     console.log("The card is now clear!");
-    const discount = "10%"; 
+    const discount = "10%";
     const uniqueNumber = `COUPON-${Math.floor(Math.random() * 100000)}`;
     setDiscountPercentage(discount);
     setCouponNumber(uniqueNumber);
     setScratchedText(`Discount ${discount}`);
-    setIsScratched(true); 
+    setIsScratched(true);
   };
 
   const settings = {
@@ -28,10 +30,12 @@ export default function ScratchCardView() {
     onComplete: handleScratchComplete,
   };
 
+  const balloons = Array.from({ length: 4 }); // Create an array with 4 balloons
+
   return (
     <div className="Apps">
       <div className="flex justify-center items-center h-screen bg-gray-100">
-        <div className="bg-white shadow-lg rounded-lg p-6 text-center">
+        <div className="bg-white shadow-lg rounded-lg p-6 text-center relative" style={{ width: "300px", height: "300px" }}>
           {!isScratched ? (
             <div className="scratchCardContainers">
               <ScratchCard {...settings}>
@@ -43,8 +47,8 @@ export default function ScratchCardView() {
           ) : (
             <div className="scratchedDiv text-lg font-bold text-gray-700">
               <div>
-                <p>Congratulations! You Won!</p>
-                <img src={bouquetImage} alt="Bouquet" style={{ maxWidth: "100%", marginTop: "20px" }} />
+              <p className="text-3xl font-bold typewriter">Congratulations! You Won!</p>
+                <img src={bouquetImage} alt="Bouquet" style={{ maxWidth: "100%", marginTop: "10px", height: "30vh", display: "block", marginLeft: "auto", marginRight: "auto" }} />
               </div>
               {scratchedText}
               {couponNumber && (
